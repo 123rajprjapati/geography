cb904e073058a0ff9b76cfac25ff967b91313c6f
raj prajapati <134362249+123rajprjapati@users.noreply.github.com>
2023-05-23 12:09:01 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,24 @@
+function display4(){
+    document.getElementById('learn-more1').style.display="block";
+}
+function displayoff4(){
+    document.getElementById('learn-more1').style.display="none";
+}
+function display3(){
+    document.getElementById('learn-more2').style.display="block";
+}
+function displayoff3(){
+    document.getElementById('learn-more2').style.display="none";
+}
+function display2(){
+    document.getElementById('learn-more1').style.display="block";
+}
+function displayoff2(){
+    document.getElementById('learn-more1').style.display="none";
+}
+function display1(){
+    document.getElementById('learn-more1').style.display="block";
+}
+function displayoff1(){
+    document.getElementById('learn-more1').style.display="none";
+}
